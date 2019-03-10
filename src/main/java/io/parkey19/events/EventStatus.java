ad5ba8cb582d515dc659d910c201473eb350dbbd
SungJinPark <kkttqq@gmail.com>
2019-03-11 00:11:28 +0900

    event enum add

---
+++ src/main/java/io/parkey19/events/EventStatus.java
@@ -0,0 +1,8 @@
+package io.parkey19.events;
+
+/**
+ * Created by parkey19 on 2019. 3. 11..
+ */
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMEND, CLOSED_ENROLLMENT, STARTED, ENDED
+}
